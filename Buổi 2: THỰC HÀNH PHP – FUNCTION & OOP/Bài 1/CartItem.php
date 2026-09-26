be2d422993c0d11992f6a36a70fd64273d4bb4a6
Nguyen Thi Hong Ngoc <23001910@hus.edu.vn>
2026-09-26 23:13:23 +0700

    Add CartItem class for shopping cart functionality

--- Buổi 2: THỰC HÀNH PHP – FUNCTION & OOP/Bài 1/CartItem.php
+++ Buổi 2: THỰC HÀNH PHP – FUNCTION & OOP/Bài 1/CartItem.php
@@ -1,1 +1,33 @@
+class CartItem
+{
+    private $name;
+    private $price;
+    private $quantity;
 
+    public function __construct($name, $price, $quantity)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    public function getTotal()
+    {
+        return $this->price * $this->quantity;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
